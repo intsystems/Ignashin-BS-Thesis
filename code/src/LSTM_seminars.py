7e7aa5512c5e7b6df590e04d8cda61d1a43df676
Igoreshka <ignashin.in@phystech.edu>
2024-03-01 01:18:41 +0300

    Add RNN from seminars

---
+++ code/src/LSTM_seminars.py
@@ -0,0 +1,86 @@
+
+
+import torch 
+import torch.nn as nn
+from torch import Tensor
+
+        
+class Encoder(torch.nn.Module):
+    @property
+    def device(self):
+        return next(self.parameters()).device
+
+    def __init__(self, vocab_size, emb_dim=30, hidden_dim=30):
+        super(Encoder, self).__init__()
+        self.embedding = torch.nn.Embedding(vocab_size, emb_dim)
+        self.lstm = torch.nn.LSTM(emb_dim, hidden_dim, batch_first=True)
+
+    def forward(self, input):
+        r'''
+        :param input: тензор размера batch_size x seq_len --- список токенов
+
+        '''
+        act = self.embedding(input)
+        act, hidden = self.lstm(act)
+        return act, hidden
+
+
+class Decoder(torch.nn.Module):
+    @property
+    def device(self):
+        return next(self.parameters()).device
+
+    def __init__(self, vocab_size, emb_dim=30, hidden_dim=30):
+        super(Decoder, self).__init__()
+        self.embedding = torch.nn.Embedding(vocab_size, emb_dim)
+        self.attention = torch.nn.MultiheadAttention(emb_dim, 1)
+
+        self.lstm = torch.nn.LSTM(emb_dim, hidden_dim, batch_first=True)
+
+        self.linear = torch.nn.Linear(hidden_dim, vocab_size)
+
+    def forward(self, input, encoder_outputs, hidden):
+        r'''
+        :param input: тезор размера batch_size x seq_len
+        '''
+        act = self.embedding(input)
+
+        act, _ = self.attention(act.transpose(0, 1), 
+                                encoder_outputs.transpose(0, 1), 
+                                encoder_outputs.transpose(0, 1))
+
+        act = act.transpose(0, 1)
+        act, hidden = self.lstm(act, hidden)
+        return self.linear(act), hidden
+
+class seq2seq(torch.nn.Module):
+    @property
+    def device(self):
+        return next(self.parameters()).device
+
+    def __init__(self, vocab_size, emb_dim=30, hidden_dim=30):
+        super(seq2seq, self).__init__()
+        self.vocab_size = vocab_size
+        self.encoder = Encoder(vocab_size, emb_dim, hidden_dim)
+        self.decoder = Decoder(vocab_size, emb_dim, hidden_dim)
+
+    def forward(self, input, decoder_input=None, max_seq_len=64):
+        r'''
+        '''
+        encoder_output, hidden = self.encoder(input)
+
+        if decoder_input is None:
+            translated_scores = torch.zeros(len(input), 
+                                            max_seq_len, 
+                                            self.vocab_size).to(self.device)
+            translated_scores[:, 0, input[:, 0]] = 1.
+            for i in range(1, max_seq_len):
+                translated_scores[:, i:i+1], hidden = self.decoder(
+                    torch.argmax(translated_scores[:, i-1:i], axis=-1), 
+                    encoder_output, 
+                    hidden)
+        else:
+            translated_scores, _ = self.decoder(
+                decoder_input, encoder_output, hidden)
+
+        return translated_scores
---
+++ code/src/train_seminars.py
@@ -0,0 +1,64 @@
+
+import torch 
+import torch.nn as nn
+from torch import Tensor
+from tqdm import tqdm
+import numpy as np
+
+def train_on_batch(model, x_batch, y_batch, optimizer, loss_function):
+    model.train()
+    optimizer.zero_grad()
+    
+    # print(x_batch.shape , y_batch.shape)
+    x_batch =x_batch.permute(1,0)
+    y_batch =y_batch.permute(1,0)
+    output = model(x_batch.to(model.device), y_batch.to(model.device))
+    
+    loss = loss_function(output.transpose(1,2), 
+                         y_batch.to(model.device))
+    loss.backward()
+
+    optimizer.step()
+    return loss.cpu().item()
+
+def train_epoch(train_generator, model, loss_function, optimizer, callback = None):
+    epoch_loss = 0
+    total = 0
+    for it, (batch_of_x, batch_of_y) in enumerate(train_generator):
+        batch_loss = train_on_batch(model, batch_of_x, batch_of_y, optimizer, loss_function)
+        
+        if callback is not None:
+            with torch.no_grad():
+                callback(model, batch_loss)
+            
+        epoch_loss += batch_loss*len(batch_of_x)
+        total += len(batch_of_x)
+    
+    return epoch_loss/total
+
+def trainer(count_of_epoch, 
+            batch_size, 
+            dataloader,
+            dataset , 
+            model, 
+            loss_function,
+            optimizer,
+            lr = 0.001,
+            callback = None):
+
+    optima = optimizer(model.parameters(), lr=lr)
+    
+    iterations = tqdm(range(count_of_epoch), desc='epoch')
+    iterations.set_postfix({'train epoch loss': np.nan})
+    for it in iterations:
+        batch_generator = tqdm(
+            dataloader, 
+            leave=False, total=len(dataset)//batch_size+(len(dataset)%batch_size>0))
+        
+        epoch_loss = train_epoch(train_generator=batch_generator, 
+                    model=model, 
+                    loss_function=loss_function, 
+                    optimizer=optima, 
+                    callback=callback)
+        
+        iterations.set_postfix({'train epoch loss': epoch_loss})
